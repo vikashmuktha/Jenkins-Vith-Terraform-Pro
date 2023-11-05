2faefd38768d548ef5cdbafbc40c05909fa7c6a9
Vikash M <111482795+vikashmuktha@users.noreply.github.com>
2023-11-05 23:37:55 +0530

    Update variables.tf
    Updating AMI id

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-00983e8a26e4c9bd9"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
